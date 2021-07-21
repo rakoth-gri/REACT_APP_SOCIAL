90ce72cc4372796836c88109b4c849e01c982083
GRI <gri@yandex.ru>
2021-07-21 16:29:36 +0300

    first__state

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+import './Header.css'
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img alt="logo" src="https://shmector.com/_ph/13/74615743.png" className="header__logo"/>
+        </header>
+    );
+} 
